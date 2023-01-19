9c0360d3bfcbcb9a5db5daac1e34d6475721e197
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-01-18 19:24:14 -0500

    got rid of useless projects

--- angular-workspace/projects/ngx-milld3/src/lib/ngx-milld3.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NgxMilld3Component } from './ngx-milld3.component';
-
-@NgModule({
-  declarations: [NgxMilld3Component],
-  imports: [],
-  exports: [NgxMilld3Component],
-})
-export class NgxMilld3Module {}
