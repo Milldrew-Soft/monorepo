5c4793b9919df6becdffb424b59a7e1566f3f486
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-01-21 12:52:31 -0500

    📅  | Sat Jan 21 12:52:31 EST 2023

--- angular-workspace/projects/webrtc-scratch-pad/src/app/app.component.ts
+++ angular-workspace/projects/webrtc-scratch-pad/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {}
+  isConnected = false;
+  callButtonInnerHtml: 'Join Call' | 'Leave Call' = 'Join Call';
+  callButtonClasses = {
+    connected: !!this.isConnected,
+    disconnected: !this.isConnected,
+  };
+  handleCallButton(event: MouseEvent) {
+    this.isConnected = !this.isConnected;
+    console.log(this.isConnected);
+    if (this.isConnected) {
+      this.callButtonInnerHtml = 'Leave Call';
+    } else {
+      this.callButtonInnerHtml = 'Join Call';
+    }
+  }
 }
