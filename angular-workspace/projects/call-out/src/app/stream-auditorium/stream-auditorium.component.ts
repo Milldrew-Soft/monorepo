33d5fedb025f0ca3d8200768be2e30f0997e6527
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-01-28 19:09:07 -0700

    📅  | Sat Jan 28 19:09:07 MST 2023

--- angular-workspace/projects/call-out/src/app/stream-auditorium/stream-auditorium.component.ts
+++ angular-workspace/projects/call-out/src/app/stream-auditorium/stream-auditorium.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-stream-auditorium',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class StreamAuditoriumComponent {
   streamId: string;
+  constructor(private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.streamId = params['streamId'];
+    });
+  }
 }
