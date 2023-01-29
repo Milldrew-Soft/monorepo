3a927766663c07ed5ee976282deaea21193ed2fa
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-01-28 19:02:10 -0700

    📅  | Sat Jan 28 19:02:10 MST 2023

--- angular-workspace/projects/call-out/src/app/app-routing.module.ts
+++ angular-workspace/projects/call-out/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { StreamAuditoriumComponent } from './stream-auditorium/stream-auditorium
 const routes: Routes = [
   { path: '', component: LandingPageComponentComponent },
   { path: 'home', component: StreamAuditoriumComponent },
+  { path: '**', component: LandingPageComponentComponent },
 ];
 
 @NgModule({
