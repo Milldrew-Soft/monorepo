10d132a74d2fea8a89372973bb36f1f4df0cedb8
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-01-29 14:54:16 -0700

    📅  | Sun Jan 29 14:54:16 MST 2023

--- angular-workspace/playwright/tests/viewport-size.spec.ts
+++ angular-workspace/playwright/tests/viewport-size.spec.ts
@@ -1,15 +1,20 @@
-import { test } from '@playwright/test';
+import { test, devices, ViewportSize } from '@playwright/test';
 
 test('visualy look at screen sizes', async ({ page }) => {
   await page.goto('http://localhost:4200/');
-  type ViewPort = { width: number; height: number };
-  const viewPorts: ViewPort[] = [
+  const viewPorts: ViewportSize[] = [
     { width: 1920, height: 1080 },
     { width: 1440, height: 900 },
     { width: 2560, height: 1440 },
     { width: 1536, height: 864 },
     { width: 1280, height: 720 },
+    devices['iPhone 11 Pro'].viewport,
+    devices['iPhone 11'].viewport,
+    devices['iPhone 8'].viewport,
+    devices['iPhone 6 landscape'].viewport,
+    devices['iPhone SE'].viewport,
   ];
+
   const viewPortCount = viewPorts.length;
   for (let viewPortId = 0; viewPortId < viewPortCount; viewPortId += 1) {
     const currentViewPort = viewPorts[viewPortId];
